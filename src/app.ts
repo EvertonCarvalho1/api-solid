9f2e0f60f6126c926fd9403af94d9004ae348281
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2023-11-09 12:30:21 -0300

    fundamentals of Prisma ORM

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
 import { fastify } from "fastify";
+import { PrismaClient } from '@prisma/client';
 
 export const app = fastify();
+
+const prisma = new PrismaClient();
+
+prisma.user.create({
+    data: {
+        name: 'Everton Carvalho',
+        email: 'everton@gmail.com',
+
+    }
+})
