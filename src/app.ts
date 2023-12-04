8842b2a872708621dd97edd34ff90e55897e02ee
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2023-12-04 12:38:55 -0300

    creating a user

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,27 @@
 import { fastify } from "fastify";
 import { z } from 'zod';
-import { PrismaClient } from '@prisma/client';
+import { prisma } from "./lib/prisma";
+
 
 export const app = fastify();
 
-app.post('/users', (request, reply) => {
+app.post('/users', async (request, reply) => {
     const registerBodySchema = z.object({
         name: z.string(),
         email: z.string().email(),
         password: z.string().min(6),
     })
 
-    const { } = registerBodySchema.parse(request.body);
+    const { name, email, password } = registerBodySchema.parse(request.body);
+
+    await prisma.user.create({
+        data: {
+            name,
+            email,
+            password_hash: password
+        }
+    });
+
+    return reply.status(201).send();
 })
 
