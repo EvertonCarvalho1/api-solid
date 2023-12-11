e0fb56287dbb97f0a649ef7566af9d1110166d15
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2023-12-11 12:16:15 -0300

    dependency inversion

--- src/repositories/prisma-users-repository.ts
+++ src/repositories/prisma-users-repository.ts
@@ -3,8 +3,12 @@ import { Prisma } from '@prisma/client'
 
 export class PrismaUsersRepository {
 
-  async create(data) {
+  async create(data: Prisma.UserCreateInput) {
+    const user = await prisma.user.create({
+      data
+    });
 
+    return user;
   }
 }
 
