01cb14a547b86e3422f91309b9b1bfbf2ecfbfb6
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2024-01-02 12:29:03 -0300

    global error handler

--- src/http/controllers/users/register.ts
+++ src/http/controllers/users/register.ts
@@ -32,7 +32,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       });
     }
 
-    return reply.status(500).send();
+    throw err;
   }
 
   return reply.status(201).send();
