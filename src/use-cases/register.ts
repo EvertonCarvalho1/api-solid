773c15c23bd3a47ec6440c9403dca8b749562410
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2024-01-02 12:24:30 -0300

    dealing with use case errors

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,6 +1,7 @@
 import { prisma } from "@/lib/prisma";
 import { UsersRepository } from "@/repositories/users-repository";
 import { hash } from "bcryptjs";
+import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
 
 interface RegisterUseCaseRequest {
     name: string;
@@ -23,7 +24,7 @@ export class RegisterUseCase {
         const userWithSameEmail = await this.usersRepository.findByEmail(email);
 
         if (userWithSameEmail) {
-            throw new Error('E-mail already exists.');
+            throw new UserAlreadyExistsError();
         }
 
         await this.usersRepository.create({
